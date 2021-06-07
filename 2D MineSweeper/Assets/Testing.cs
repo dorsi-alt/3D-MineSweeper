2e0dd30d39e51698d6f0e06559d668e5884ec3d4
dorsi-alt <orsidaniel03@gmail.com>
2021-06-06 21:21:46 -0400

    grid and testing

---
+++ 2D MineSweeper/Assets/Testing.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    Grid grid;
+    int value;
+    private void Start()
+    {
+        grid = new Grid(4, 2, 10f, new Vector3(20, 0));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition), 10);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        }
+    }
+}
